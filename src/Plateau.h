c277efcb1fadf9c915eb9b53ed8c5ede3a55f83c
alexbien <alexandre.bienfait@ulb.be>
2023-10-25 19:08:45 +0200

    remove unused function

--- src/Plateau.h
+++ src/Plateau.h
@@ -25,26 +25,11 @@ class Plateau
         bool ajouterPiece(string nom, char couleur);
         bool ajouterPieceVirtuelle(string nom, char couleur);
         bool verifie_la_prise(Case* c, Case* (Case::*setNeighbor)());
-        bool verifie_la_prise_Up(Case* c);
-        bool verifie_la_prise_Down(Case* c);
-        bool verifie_la_prise_Left(Case* c);
-        bool verifie_la_prise_Right(Case* c);
-        bool verifie_la_prise_UpLeft(Case* c);
-        bool verifie_la_prise_UpRight(Case* c);
-        bool verifie_la_prise_DownLeft(Case* c);
-        bool verifie_la_prise_DownRight(Case* c);
 
         void capturePiece(Case* c, Case* (Case::*getNeighbor)());
         bool capturePieces(Case* c);
         bool capturePiecesVirtuelle(Case* c);
-        void capturePieceUp(Case* c);
-        void capturePieceDown(Case* c);
-        void capturePieceLeft(Case* c);
-        void capturePieceRight(Case* c);
-        void capturePieceUpLeft(Case* c);
-        void capturePieceUpRight(Case* c);
-        void capturePieceDownLeft(Case* c);
-        void capturePieceDownRight(Case* c);
+
         bool verifie_si_le_joueur_peut_jouer(char couleur);
         bool passe_le_tour();
         bool fin_de_partie();
